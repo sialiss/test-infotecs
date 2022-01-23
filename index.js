e42d2896cc4d1a59f211b41ced311c37a97e2934
Camellia <56206613+sialiss@users.noreply.github.com>
2022-01-23 22:20:50 +0300

    add comments

--- index.js
+++ index.js
@@ -4,25 +4,33 @@
 
 const table = document.getElementById("table")
 let quantLine = 10
-const columns = ["firstName", "lastName", "about", "eyeColor"]
+const columns = {
+    "firstName" : "Имя", "lastName" : "Фамилия",
+    "about" : "Описание", "eyeColor" : "Цвет глаз"}
 
 // преобразование данных из json в массив объектов
+// для использования fetch нужен сервер
 const data = await fetch('./data.json').then(res => res.json())
 
 function createTable(table, columns) {
+    // Создание головы таблциы
     const thead = document.createElement("thead")
     table.append(thead)
     const tr = document.createElement("tr")
     thead.append(tr)
-    for (const column of columns) {
+
+    for (const column of Object.getOwnPropertyNames(columns)) {
+        // Создание и заполнение заголовков таблицы
         const th = document.createElement("th")
-        th.innerText = column
+        th.innerText = columns[column]
         tr.append(th)
     }
     fillTable(table, columns, data)
 }
 
 function fillTable(table, columns, data) {
+    // Заполняет таблицу
+    // Создаёт тело таблицы
     const tbody = document.createElement("tbody")
     table.append(tbody)
 
@@ -34,23 +42,36 @@ function fillTable(table, columns, data) {
 }
 
 function fillRow(tr, each, columns) {
-    for (const column of columns) {
+    // Заполняет данные для каждого объекта
+    for (const column of Object.getOwnPropertyNames(columns)) {
+        // Создаёт ячейку (столбик)
         const td = document.createElement("td")
         tr.append(td)
         if (column == 'about') {
+            // добавляет колонке about css класс (для скрытия информации)
             td.classList.add('about')
         }
         if (each[column] != undefined) {
+            // если у объекта json есть колонка с нужным названием,
+            // то ячейка заполняется
             td.innerText = each[column]
         }
         else {
-            for (const name of Object.getOwnPropertyNames(each)) {
-                if (each[name][column]) {
-                    td.innerText = each[name][column]
-                }
-            }
+            // если колонка не определена, 
+            // то проверяет есть ли нужные данные в свойствах
+            td.innerText = getFromProperties(each, column)
         }
     }
+}
+
+function getFromProperties(obj, nedeedData) {
+    // проходит по всем свойствам объекта json, 
+    // если находит, возвращает необходимую информацию
+    for (const name of Object.getOwnPropertyNames(obj)) {
+                if (obj[name][nedeedData]) {
+                    return obj[name][nedeedData]
+                }
+            }
 }
 
 createTable(table, columns)
